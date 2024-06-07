6ea52520c45d91a94b7f5c8ad052f0dbdd460cbc
Avsar2001 <voraavsar@gmail.com>
2024-06-07 17:24:30 +0200

    project work 5

---
+++ project/tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Export kaggle.json to os env for Kaggle authentication
+KAGGLE_JSON_PATH="./kaggle.json"
+KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
+export KAGGLE_CONFIG_DIR
+
+# Install required packages from requirements.txt
+pip install --upgrade pip
+pip install -r ../requirements.txt
+
+# Run your Python test file
+pytest ./test_data_collection_pipeline.py
+
+echo "pytest script has been completed"
